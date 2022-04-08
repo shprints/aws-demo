769645fb1d873aedd897dc5f416728ceae5e4fe6
k.shprints <k.shprints@itransition.com>
2022-04-08 16:16:35 +0300

    init2

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static java.time.LocalTime.now;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            System.out.println("kek: " + now());
+    }
+}
